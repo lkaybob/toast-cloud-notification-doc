9f3e67eac38e577451ecb57f05a72b9539d97283
Lawrence Kay <lkay9495@hotmail.com>
2019-10-05 01:32:39 +0900

    Update SMS Documentation * In order to split SMS SDK and Mail SDK

--- generate-composer-package.sh
+++
@@ -1,2 +0,0 @@
-#/bin/sh
-swagger-codegen generate -i toast-sms.yaml -l php -o ../ -c config.json
---
+++ generate-sms-composer-package.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+swagger-codegen generate -i toast-sms.yaml -l php -o ../ -c sms-composer-config.json
